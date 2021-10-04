ea4c5d3fc12bbfc30cbe42c3ae4626164300e447
鬼木　良輔 <ryosuke@onikiryousukenoMacBook-Air-3.local>
2021-10-04 10:07:51 +0900

    create commons views

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,25 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">AdmiGames</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    {{-- ログアウトへのリンク --}}
+                    {!! link_to_route('logout.get', 'Logout') !!}
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
+            </ul>
+        </div>
+    </nav>
+</header>
